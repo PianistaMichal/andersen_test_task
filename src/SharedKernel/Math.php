a1ce6c678eb20b2e686df6be3b93a9d3d4589151
Administrator <pianistamichal@gmail.com>
2021-06-23 14:32:50 +0200

    Added fixing for rounding

--- src/SharedKernel/Math.php
+++ src/SharedKernel/Math.php
@@ -6,48 +6,52 @@ namespace App\SharedKernel;
 
 class Math
 {
-    private static int $BASIC_SCALE_FOR_CALCULATIONS = 20;
+    private const BASIC_SCALE_FOR_CALCULATIONS = 20;
+    private int $basicScaleForCalculations;
     private int $scale;
 
     public function __construct(int $scale)
     {
+        $this->basicScaleForCalculations = $scale + self::BASIC_SCALE_FOR_CALCULATIONS;
         $this->scale = $scale;
     }
 
     public function add(string $leftOperand, string $rightOperand): string
     {
-        return bcadd($leftOperand, $rightOperand, self::$BASIC_SCALE_FOR_CALCULATIONS);
+        return bcadd($leftOperand, $rightOperand, $this->basicScaleForCalculations);
     }
 
     public function multiply(string $leftOperand, string $rightOperand): string
     {
-        return bcmul($leftOperand, $rightOperand, self::$BASIC_SCALE_FOR_CALCULATIONS);
+        return bcmul($leftOperand, $rightOperand, $this->basicScaleForCalculations);
     }
 
     public function divide(string $leftOperand, string $rightOperand): string
     {
-        return bcdiv($leftOperand, $rightOperand, self::$BASIC_SCALE_FOR_CALCULATIONS);
+        return bcdiv($leftOperand, $rightOperand, $this->basicScaleForCalculations);
     }
 
     public function sub(string $leftOperand, string $rightOperand): string
     {
-        return bcsub($leftOperand, $rightOperand, self::$BASIC_SCALE_FOR_CALCULATIONS);
+        return bcsub($leftOperand, $rightOperand, $this->basicScaleForCalculations);
     }
 
     public function comp(string $leftOperand, string $rightOperand): int
     {
-        return bccomp($leftOperand, $rightOperand, self::$BASIC_SCALE_FOR_CALCULATIONS);
+        return bccomp($leftOperand, $rightOperand, $this->basicScaleForCalculations);
     }
 
     /**
-     * https://www.php.net/manual/en/function.round.php#114573.
+     * Should use this function only when saving or displaying. Otherwise use functions from above
+     *
+     * https://stackoverflow.com/questions/8239600/rounding-up-to-the-second-decimal-place/8239620#comment67989595_8239620
      */
     public function round(string $number): string
     {
-        $pow = pow(10, $this->scale);
-        $ceil = (string) ((ceil($pow * (float) $number) + ceil(
-                    $pow * (float) $number - ceil($pow * (float) $number)
-                )) / $pow);
+        $offset = 0.5;
+        if ($this->scale !== 0)
+            $offset /= pow(10, $this->scale);
+        $ceil = (string) round((float)$number + $offset, $this->scale, PHP_ROUND_HALF_DOWN);
 
         return bcadd($ceil, '0', $this->scale);
     }
